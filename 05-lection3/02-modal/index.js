4b6593ac3a781b1d893bf10dee38a4a722774457
Dmitry Lebedko <dimeliora710@yandex.ru>
2024-10-03 23:56:08 +0300

    feat: add a modal component

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,36 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function handleModal() {
+    const
+        modal = document.getElementById('modal'),
+        openBtn = document.getElementById('modal-open'),
+        closeBtn = document.getElementById('modal-close');
 
-    if (!close || !modalOpen || !modal) {
-        return;
+    if (!closeBtn || !openBtn || !modal) {
+        throw new ReferenceError('Some of the elements were not found');
     }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
+    const openModal = () => {
+        modal.classList.add('modal_open');
         document.body.overflow = 'hidden';
+    };
+
+    const closeModal = () => {
+        modal.classList.remove('modal_open');
+        document.body.overflow = 'initial';
+    };
+
+    openBtn.addEventListener('click', openModal);
+
+    closeBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.closest('.modal__content') == null) {
+            closeModal();
+        }
     });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            closeModal();
+        }
     });
 })();
